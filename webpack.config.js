d6b09b1b3db38fbbe24a9c62346e19e90274a817
henrique elias <hcbe2004@gmail.com>
2018-12-22 11:12:26 -0200

    changes on webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const devMode = process.env.NODE_ENV !== "production";
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -8,14 +10,27 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: devMode ? "[name].css" : "[name].[hash].css",
+      chunkFilename: devMode ? "[id].css" : "[id].[hash].css"
+    })
+  ],
   module: {
     rules: [
       {
         test: /\.(c|le)ss$/, // less or CSS
         use: [
-          { loader: "style-loader" },
+          { loader: devMode ? "style-loader" : MiniCssExtractPlugin.loader },
           { loader: "css-loader" },
-          { loader: "less-loader" }
+          {
+            loader: "less-loader",
+            options: {
+              javascriptEnabled: true
+            }
+          }
         ]
       }
     ]
