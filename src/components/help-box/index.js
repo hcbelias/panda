faa66ce33f7699343fe9c2cd6a38d7cf756524bd
henrique elias <hcbe2004@gmail.com>
2018-12-26 22:16:56 -0200

    updates on help box

--- src/components/help-box/index.js
+++ src/components/help-box/index.js
@@ -1,11 +1,9 @@
 import './style.scss';
 
 class HelpBox extends HTMLElement {
-
   constructor() {
     super();
   }
-
 
   connectedCallback() {
     this.render();
@@ -13,8 +11,40 @@ class HelpBox extends HTMLElement {
 
   render() {
     this.innerHTML = require('./template.pug');
-  }
+    const helpButtonId = 'help-box-header-button';
+    let helpHeader = document.getElementById(helpButtonId);
+    helpHeader.addEventListener(
+      'click',
+      function() {
+        const openAttribute = 'is-open';
+        let isOpen = !helpHeader.getAttribute(openAttribute);
+        const openContentClassName = 'help-box__content-open';
+        let helpBoxContentContainer = document.getElementById(
+          'help-box-content-container'
+        );
+        helpHeader.innerText = '';
 
+        const text = isOpen ? 'Close' : 'Open';
+        const cssClass = isOpen ? 'fa-long-arrow-up' : 'fa-long-arrow-down';
+        const textSpan = document.createElement('span');
+        textSpan.innerText = text;
+        const icon = document.createElement('i');
+        icon.setAttribute('id', helpButtonId);
+        icon.classList.add('fa');
+        icon.classList.add(cssClass);
+        helpHeader.appendChild(textSpan);
+        helpHeader.appendChild(icon);
+
+        if (isOpen) {
+          helpHeader.setAttribute(openAttribute, true);
+          helpBoxContentContainer.classList.add(openContentClassName);
+        } else {
+          helpHeader.removeAttribute(openAttribute);
+          helpBoxContentContainer.classList.remove(openContentClassName);
+        }
+      }.bind(this)
+    );
+  }
 }
 
 export default HelpBox;
