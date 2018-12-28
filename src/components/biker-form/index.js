97a9303b555c164a269a2dfd322124c48e3d55a6
henrique elias <hcbe2004@gmail.com>
2018-12-28 08:21:43 -0200

    fixes for biker-form

--- src/components/biker-form/index.js
+++ src/components/biker-form/index.js
@@ -1,10 +1,14 @@
 import './style.scss';
-const saveButtonId = 'biker-form__saveButton';
-const cancelButtonId = '#biker-form__cancelButton';
+
+const cancelButtonId = 'biker-form__cancelButton';
+const bikerFormId = 'biker-form';
+
+
 
 class BikerForm extends HTMLElement {
   constructor() {
     super();
+    
   }
 
   connectedCallback() {
@@ -13,10 +17,64 @@ class BikerForm extends HTMLElement {
 
   render() {
     this.innerHTML = require('./template.pug');
-    //  let helpHeader = document.getElementById(helpButtonId);
-    //helpHeader.addEventListener("click", this.collapseHelperBox(helpHeader));
+    let cancelButton = document.getElementById(cancelButtonId);
+    let bikerForm = document.getElementById(bikerFormId);
+    bikerForm.addEventListener(
+      'submit',
+      function(event) {
+        event.preventDefault();
+        this.onSaveEvent(bikerForm);
+      }.bind(this)
+    );
+
+    cancelButton.addEventListener(
+      'click',
+      function(event) {
+        event.preventDefault();
+        this.onCancelButton(bikerForm);
+      }.bind(this)
+    );
   }
 
+  onSaveEvent(form) {
+    let inputList = form.getElementsByTagName('input');
+    let newRow = {
+      name: inputList.name.value,
+      email: inputList.email.value,
+      city: inputList.city.value,
+      rideInGroup: getRideInGroup(inputList),
+      daysOfTheWeek: getDaysOfTheWeek(inputList)
+    };
+    let tableList = document.getElementById('dynamicTable');
+    let TableList = require('./../table-list');
+    
+    debugger;
+    return false;
+  }
+
+  onCancelButton(form) {
+    
+    return false;
+  }
+}
+
+function getRideInGroup(inputList){
+  if(inputList.radio_0.checked) return 0;
+  if(inputList.radio_1.checked) return 1;
+  if(inputList.radio_2.checked) return 2;
+  return ' ';
+}
+
+function getDaysOfTheWeek(inputList){
+  let days = [];
+  if(inputList.check_0.checked) days.push(0);
+  if(inputList.check_1.checked) days.push(1);
+  if(inputList.check_2.checked) days.push(2);
+  if(inputList.check_3.checked) days.push(3);
+  if(inputList.check_4.checked) days.push(4);
+  if(inputList.check_5.checked) days.push(5);
+  if(inputList.check_6.checked) days.push(6);
+  return days;
 }
 
 export default BikerForm;
--- src/components/table-list/index.js
+++ src/components/table-list/index.js
@@ -1,43 +1,5 @@
 import './style.scss';
-//private methods
-function addRow(table) {
-  let newRow = table.insertRow();
-  newRow.classList.add('table-list__tableRow');
-  return newRow;
-}
-
-function addColumn(row, value) {
-  let column = row.insertCell();
-  column.classList.add('table-list__tableColumn');
-  column.innerText = value;
-  return column;
-}
-
-function addDeleteColumn(row, index) {
-  let column = row.insertCell();
-  column.classList.add('table-list__tableDeleteColumn');
-  let buttonDelete = document.createElement('i');
-  buttonDelete.classList.add('fa');
-  buttonDelete.classList.add('fa-trash-o');
-  buttonDelete.setAttribute('title', `Remove row: ${index + 1}`);
-  column.appendChild(buttonDelete);
-  return column;
-}
-
-function addDateColumn(row, date) {
-  let column = row.insertCell();
-  column.classList.add('table-list__tableColumn');
-  let spanDate = document.createElement('span');
-  spanDate.classList.add('table-list__dateColumn');
-  spanDate.innerText = date.toLocaleDateString('en-US');
-  let spanTime = document.createElement('span');
-  spanTime.classList.add('table-list__timeColumn');
-  spanTime.innerText = date.toLocaleTimeString('en-US');
-
-  column.appendChild(spanDate);
-  column.appendChild(spanTime);
-  return column;
-}
+const tableListId = 'dynamicTable';
 
 function getRideInGroupLabel(value) {
   switch (value) {
@@ -114,24 +76,66 @@ class TableList extends HTMLElement {
   render() {
     this.innerHTML = require('./template.pug');
     let { data } = require('./data.json'); // getting property data from external source
-    let table = document.getElementById('dynamicTable');
+    let table = document.getElementById(tableListId);
     let tableBody = table.createTBody();
     data.forEach((item, index) => {
-      let newRow = addRow(tableBody);
-      addColumn(newRow, item.name);
-      addColumn(newRow, item.email);
-      addColumn(newRow, item.city);
-      addColumn(newRow, getRideInGroupLabel(item.rideInGroup));
-      addColumn(newRow, getDaysOfTheWeekLabel(item.daysOfTheWeek));
-      let date = new Date(item.registrationDay);
-      addDateColumn(newRow, date);
-      let deleteButton = addDeleteColumn(newRow, index);
-      let clickFunction = function customClickFunction(event) {
-        event.currentTarget.parentElement.remove();
-      };
-
-      deleteButton.addEventListener('click', clickFunction);
+      this.createRowData(tableBody, item, index);
     });
+  }
+
+  createRowData(tableBody, item, index) {
+    let newRow = this.addRow(tableBody);
+    this.addColumn(newRow, item.name);
+    this.addColumn(newRow, item.email);
+    this.addColumn(newRow, item.city);
+    this.addColumn(newRow, getRideInGroupLabel(item.rideInGroup));
+    this.addColumn(newRow, getDaysOfTheWeekLabel(item.daysOfTheWeek));
+    let date = new Date(item.registrationDay);
+    this.addDateColumn(newRow, date);
+    let deleteButton = this.addDeleteColumn(newRow, index);
+    let clickFunction = function customClickFunction(event) {
+      event.currentTarget.parentElement.remove();
+    };
+    deleteButton.addEventListener('click', clickFunction);
+  }
+
+  addRow(table) {
+    let newRow = table.insertRow();
+    newRow.classList.add('table-list__tableRow');
+    return newRow;
+  }
+
+  addColumn(row, value) {
+    let column = row.insertCell();
+    column.classList.add('table-list__tableColumn');
+    column.innerText = value;
+    return column;
+  }
+
+  addDeleteColumn(row, index) {
+    let column = row.insertCell();
+    column.classList.add('table-list__tableDeleteColumn');
+    let buttonDelete = document.createElement('i');
+    buttonDelete.classList.add('fa');
+    buttonDelete.classList.add('fa-trash-o');
+    buttonDelete.setAttribute('title', `Remove row: ${index + 1}`);
+    column.appendChild(buttonDelete);
+    return column;
+  }
+
+  addDateColumn(row, date) {
+    let column = row.insertCell();
+    column.classList.add('table-list__tableColumn');
+    let spanDate = document.createElement('span');
+    spanDate.classList.add('table-list__dateColumn');
+    spanDate.innerText = date.toLocaleDateString('en-US');
+    let spanTime = document.createElement('span');
+    spanTime.classList.add('table-list__timeColumn');
+    spanTime.innerText = date.toLocaleTimeString('en-US');
+
+    column.appendChild(spanDate);
+    column.appendChild(spanTime);
+    return column;
   }
 }
 
